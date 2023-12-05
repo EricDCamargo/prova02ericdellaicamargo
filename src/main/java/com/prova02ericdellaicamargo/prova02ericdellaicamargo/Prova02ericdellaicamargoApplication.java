d11e6f57dd5340c7cb5b46620400079268d960dc
EricDCamargo <ericdcamargo@gmail.com>
2023-12-05 08:41:07 -0300

    initial commit

---
+++ src/main/java/com/prova02ericdellaicamargo/prova02ericdellaicamargo/Prova02ericdellaicamargoApplication.java
@@ -0,0 +1,13 @@
+package com.prova02ericdellaicamargo.prova02ericdellaicamargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova02ericdellaicamargoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova02ericdellaicamargoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prova02ericdellaicamargo/prova02ericdellaicamargo/Prova02ericdellaicamargoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prova02ericdellaicamargo.prova02ericdellaicamargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova02ericdellaicamargoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
